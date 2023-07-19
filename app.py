0a6964f03d8b709996e5b4935c45675e856db478
Julian Steiner <steinerj.ai@posteo.de>
2023-07-19 15:26:47 +0200

    Add cert function.

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+"""
+"""
+
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +11,9 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    cert_file = os.getenv('FLASK_CERT', '') # fullchain.pem
+    key_file = os.getenv('FLASK_CERT_KEY', '') # privatekey.pem
+
+    ssl_context = (cert_file, key_file)
+
+    app.run(host='0.0.0.0', ssl_context=ssl_context)
