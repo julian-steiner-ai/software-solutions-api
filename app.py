56181dbb0b72d2977c0f85dbe226fb13eafa79b2
Julian Steiner <steinerj.ai@posteo.de>
2023-07-19 15:43:06 +0200

    Test cowfeedcalculator.

--- app.py
+++ app.py
@@ -3,12 +3,18 @@
 
 import os
 from flask import Flask
+from flask import Response
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/cowfeedcalculator')
+def cowfeedcalculator():
+    return jsonify([{'Name': 'Julian'}, {'Name': 'Julian'}, {'Name': 'Julian'}])
 
 if __name__ == "__main__":
     cert_file = os.getenv('FLASK_CERT', '') # fullchain.pem
