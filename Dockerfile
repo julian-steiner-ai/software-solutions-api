c7a0fa5a4ca5974dccf64f05b194459d1bae09e3
Julian Steiner <steinerj.ai@posteo.de>
2023-07-29 16:44:19 +0200

    Implement HTTPS gunicorn support.

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,25 @@
-FROM python:3.9-slim-bullseye
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.10-slim
+
+EXPOSE 5002
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Install pip requirements
+COPY requirements.txt .
+RUN python -m pip install -r requirements.txt
 
 WORKDIR /app
+COPY . /app
 
-RUN apt-get -y update
-RUN apt-get -y upgrade
+# Creates a non-root user with an explicit UID and adds permission to access the /app folder
+# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+USER appuser
 
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-
-COPY app.py /app/app.py
-COPY cowfeedcalculator/ /app/cowfeedcalculator/ 
-
-CMD ["flask", "run", "--host", "0.0.0.0"]
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["gunicorn", "--certfile", "/app/cert/fullchain1.pem", "--keyfile", "/app/cert/privkey1.pem", "--bind", "0.0.0.0:5002", "app:app"]
--- app.py
+++ app.py
@@ -1,56 +1,8 @@
-"""
-Julian Steiner Software Solutions Backend.
-"""
-
-import os
-
-from OpenSSL import SSL
-
-from datetime import date
-from flask import Flask, request
+from flask import Flask
 from flask import jsonify
-from flask_cors import CORS
-
-from cowfeedcalculator.cowfeedcalculator import CowFeedCalculator
 
 app = Flask(__name__)
 
-CORS(app)
-
-@app.route('/cowfeedcalculator/calculations', methods=["GET", "POST"])
-def cowfeedcalculator():
-    """
-    Return the History for the Cow Feed Calculator.
-    """
-    return jsonify(CowFeedCalculator().get_all_calculations(os.getenv('COWFEEDCALC_HISTORY_DIR', '/')))
-
-@app.route('/cowfeedcalculator/calculation/save', methods=['POST'])
-def save_calculation():
-    """
-    Save a new entry for the Cow Feed Calculator.
-    """
-    directory = os.getenv('COWFEEDCALC_HISTORY_DIR', '/')
-    filename=f'{date.today().strftime("%d%m%Y")}.json'
-
-    feed_types = ''
-    if 'feedTypes' in request.json: 
-        feed_types = request.json['feedTypes']
-
-    return jsonify(CowFeedCalculator().save_calculation(
-        directory=directory,
-        filname=filename,
-        json_content=feed_types
-    ))
-
-if __name__ == "__main__":
-    key_file = os.getenv('PRIVKEY_FILE', '') #privkey
-    fullchain_file = os.getenv('FULLCHAIN_FILE', '') #fullchain
-    cert_file = os.getenv('CERT_FILE', '') #cert
-
-    context = SSL.Context(SSL.TLS1_3_VERSION)
-
-    context.use_privatekey_file(key_file)
-    context.use_certificate_chain_file(fullchain_file)
-    context.use_certificate_file(cert_file)
-
-    app.run(debug=True, ssl_context=context)
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({"running": True})
